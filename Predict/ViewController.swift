113170a24903eb8c01cce980de8bca5a4ca3925e
letJamie <stillbut@naver.com>
2020-08-21 22:40:21 +0900

    Set up UI and create ML

--- Predict/ViewController.swift
+++ Predict/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var sentimentalLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func predictPressed(_ sender: UIButton) {
+    }
+    
 }
 
