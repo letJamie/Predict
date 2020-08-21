a53ddf5e5f246d241fcf29306928d471a3cca35e
letJamie <stillbut@naver.com>
2020-08-21 16:25:46 +0900

    Initial Commit

---
+++ Predict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Predict
+//
+//  Created by Jamie on 2020/08/21.
+//  Copyright © 2020 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
